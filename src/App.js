f8e8482697f97d596298d6b4937b43fb5291727d
DexterFLN <dragon_dexter-x@yahoo.com.br>
2020-08-20 23:33:18 -0300

    tests done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
-  }
+    const response = await api.post('repositories', {
+      title: `Novo repositório ${Date.now()}`,
+      url: 'https://github.com/DexterFLN/conceitos-reactjs',
+      techs: ['Java', 'JS', 'ReactJS']
+    });
+
+    const repository = response.data;
+
+    setRepositories([ ...repositories, repository]);
+  };
 
   async function handleRemoveRepository(id) {
-    // TODO
-  }
+      await api.delete(`repositories/${id}`); //o comando delete não retorna nada, por isso não é utilizado o const response
+  };
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button type="button" onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button type="button" onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
